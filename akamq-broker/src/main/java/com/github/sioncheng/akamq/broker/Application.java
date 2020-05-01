555c1df294b6aada17c4f89e21679cd98d71d89d
sion <sion@SionMacBookPro.local>
2020-05-01 17:10:17 +0800

    add broker module

---
+++ akamq-broker/src/main/java/com/github/sioncheng/akamq/broker/Application.java
@@ -0,0 +1,13 @@
+package com.github.sioncheng.akamq.broker;
+
+/**
+ * @author cyq
+ * @create 2020-05-01 5:04 PM
+ */
+public class Application {
+
+    public static void main(String[] args) {
+
+        System.out.println("akamq broker");
+    }
+}
