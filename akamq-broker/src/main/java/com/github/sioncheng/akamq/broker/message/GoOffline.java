27582f7f7b0c74261ba85fdd2748d5b2f0eb5bbf
sion <sion@SionMacBookPro.local>
2020-08-19 13:57:38 +0800

    fix lombok issue for class GoOffline

--- akamq-broker/src/main/java/com/github/sioncheng/akamq/broker/message/GoOffline.java
+++ akamq-broker/src/main/java/com/github/sioncheng/akamq/broker/message/GoOffline.java
@@ -1,5 +1,6 @@
 package com.github.sioncheng.akamq.broker.message;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -11,5 +12,6 @@ import lombok.NoArgsConstructor;
 @Builder
 @Data
 @NoArgsConstructor
+@AllArgsConstructor
 public class GoOffline {
 }
